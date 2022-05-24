6ecc70bdfae19cf3bcb093682c6d414341a91c9d
duongkma <thucduong1199@gmail.com>
2022-05-24 15:39:58 +0700

    Xây dựng UI phần Header #4

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -5,15 +5,32 @@ import { useState, useEffect } from 'react'
 
 //fonts
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons'
 
 import Tippy from '@tippyjs/react/headless'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import AccountItem from '~/components/AccountItem'
 import Button from '~/components/Button'
+import Menu from '~/components/Popper/Menu'
 
 
 const cx = classNames.bind(styles)
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and Help',
+        to: '/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard Shorcuts'
+    },
+]
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
@@ -30,7 +47,7 @@ function Header() {
 
             <Tippy
                 interactive
-                visible={searchResult.length>0}
+                visible={searchResult.length > 0}
                 render={attrs => (
                     <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                         <PopperWrapper>
@@ -46,12 +63,12 @@ function Header() {
                 )}
             >
                 <div className={cx('search')}>
-                    <input placeholder="Search accounts and videos" spellCheck={false}/>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
                     <button className={cx('clear-btn')}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                     <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                    
+
                     <button className={cx('search-btn')}>
                         <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
@@ -60,6 +77,15 @@ function Header() {
             <div className={cx('actions')}>
                 <Button text>Upload</Button>
                 <Button primary >Log in</Button>
+
+                <Menu
+                    items={MENU_ITEMS}
+                >
+                    <button className={cx('more-btn')}>
+                        <FontAwesomeIcon icon={faEllipsisVertical} />
+                    </button>
+                </Menu>
+                
             </div>
         </div>
     </header>
